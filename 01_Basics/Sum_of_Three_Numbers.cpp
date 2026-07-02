1dd2e0c71001fda23cd09da25495ca5c3b2d6dca
katyaini sharma <katyainisharma0909@gmail.com>
2026-07-02 18:26:07 +0530

    Add solution for Sum of Three Numbers

---
+++ 01_Basics/Sum_of_Three_Numbers.cpp
@@ -0,0 +1,20 @@
+/*
+Problem: Solve Me First (Sum of Three Numbers)
+Platform: HackerRank
+Topic: Basics
+Language: C++
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << a + b + c;
+
+    return 0;
+}
