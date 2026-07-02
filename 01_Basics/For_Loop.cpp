1bdb9d43243a3f32e087ed995acb606e732b9e81
katyaini sharma <katyainisharma0909@gmail.com>
2026-07-02 18:33:06 +0530

    Add solution for For Loop

---
+++ 01_Basics/For_Loop.cpp
@@ -0,0 +1,49 @@
+/*
+Problem: For Loop
+Platform: HackerRank
+Topic: Basics
+Language: C++
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Author: Katyaini Sharma
+Date: 02 July 2026
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    for (int i = a; i <= b; i++) {
+        if (i == 1)
+            cout << "one";
+        else if (i == 2)
+            cout << "two";
+        else if (i == 3)
+            cout << "three";
+        else if (i == 4)
+            cout << "four";
+        else if (i == 5)
+            cout << "five";
+        else if (i == 6)
+            cout << "six";
+        else if (i == 7)
+            cout << "seven";
+        else if (i == 8)
+            cout << "eight";
+        else if (i == 9)
+            cout << "nine";
+        else if (i % 2 == 0)
+            cout << "even";
+        else
+            cout << "odd";
+
+        cout << endl;
+    }
+
+    return 0;
+}
